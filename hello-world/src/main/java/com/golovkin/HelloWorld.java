9877c996a4907a49186c5c99c3c9bda0385cf3dd
Nikita Golovkin <nickgolovkin@gmail.com>
2021-01-15 19:07:21 +0300

    В модуль hello-world добавлена зависимость на модуль core

--- hello-world/src/main/java/com/golovkin/HelloWorld.java
+++ hello-world/src/main/java/com/golovkin/HelloWorld.java
@@ -2,6 +2,9 @@ package com.golovkin;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        int number = 10;
+        boolean isOdd = ModChecker.isOdd(number);
         System.out.println("Hello World!");
+        System.out.printf("Is %d odd: %s\n", number, isOdd);
     }
 }
