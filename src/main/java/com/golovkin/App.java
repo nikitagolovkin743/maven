eaf9b54d8974a19815de8354f2a0f9fc2de5362c
Nikita Golovkin <nickgolovkin@gmail.com>
2021-01-15 18:28:13 +0300

    init

---
+++ src/main/java/com/golovkin/App.java
@@ -0,0 +1,13 @@
+package com.golovkin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
